f596e789551fb8073eac3fa105d3c273ed143040
larryvsdata <larryvsdata@gmail.com>
2019-05-01 10:01:36 -0700

    Shell Sort Started

---
+++ ShellSort/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] intArray= {20,35,-15,7,55,1,-22};
+		
+
+
+		
+		
+		for(int i = 0 ; i < intArray.length ; i++) {
+			System.out.println(intArray[i]);
+		}
+
+	}
+	
+
+
+}
